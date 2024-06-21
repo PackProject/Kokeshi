8b5acd8cfc7c62c5fb34defa3a7de7877ac5287a
kiwi515 <49212064+kiwi515@users.noreply.github.com>
2024-06-21 19:26:57 -0400

    RPSysSelectedScene docs

--- include/Pack/RPSystem.h
+++ include/Pack/RPSystem.h
@@ -14,13 +14,13 @@
 #include <RPSystem/RPSysPlayerMgr.h>
 #include <RPSystem/RPSysPlayerSelectScene.h>
 #include <RPSystem/RPSysProjectLocal.h>
-#include <RPSystem/RPSysQueuedScene.h>
 #include <RPSystem/RPSysResourceManager.h>
 #include <RPSystem/RPSysSaveData.h>
 #include <RPSystem/RPSysSaveDataMgr.h>
 #include <RPSystem/RPSysScene.h>
 #include <RPSystem/RPSysSceneCreator.h>
 #include <RPSystem/RPSysSceneMgr.h>
+#include <RPSystem/RPSysSelectedScene.h>
 #include <RPSystem/RPSysSystem.h>
 #include <RPSystem/RPSysTagParms.h>
 
---
+++ include/Pack/RPSystem/RPSysSelectedScene.h
@@ -0,0 +1,59 @@
+#ifndef RP_SYSTEM_SELECTED_SCENE_H
+#define RP_SYSTEM_SELECTED_SCENE_H
+#include <Pack/RPTypes.h>
+#include <egg/core.h>
+
+//! @addtogroup rp_system
+//! @{
+
+/**
+ * @brief Records scene chosen from the main menu
+ */
+class RPSysSelectedScene {
+    RP_SINGLETON_DECL_EX(RPSysSelectedScene);
+
+public:
+    /**
+     * @brief Clears the selected scene ID
+     * @details The ID is set to -1.
+     */
+    void reset();
+
+    /**
+     * @brief Gets the ID of the selected scene
+     * @note The value is -1 if no scene has been selected yet.
+     */
+    s32 getSceneID() const {
+        return mSceneID;
+    }
+    /**
+     * @brief Sets the ID of the selected scene
+     *
+     * @param id Scene ID
+     */
+    void setSceneID(s32 id) {
+        mSceneID = id;
+    }
+
+private:
+    /**
+     * @brief Constructor
+     *
+     * @param pHeap Parent heap
+     */
+    RPSysSelectedScene(EGG::Heap* pHeap);
+    /**
+     * @brief Destructor
+     */
+    virtual ~RPSysSelectedScene();
+
+private:
+    //! Heap in which this object was allocated
+    EGG::Heap* mParentHeap; // at 0x4
+    //! ID of the scene that was selected from the main menu
+    s32 mSceneID; // at 0x8
+};
+
+//! @}
+
+#endif
